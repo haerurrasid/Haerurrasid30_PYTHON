fc2d93778d5dd8da60cfc03c3cd6daf634e9a5ee
haerurrasid <145946530+haerurrasid@users.noreply.github.com>
2024-05-09 19:14:36 +0800

    Add files via upload

---
+++ pertemuan04.py
@@ -0,0 +1,52 @@
+# inputan
+# aritmatika + - x : 
+
+# coba = input ("masukan nama :")
+# print (coba)
+
+#bilanagn_random = input("masukan bilanagan acak :")
+#print (bilanagn_random)
+#contoh
+
+coba = input ("masukan nama :")
+print(coba)
+
+# aritmatika
+x = 12
+y = 3
+
+# penjumlahan
+hasil = x + y
+print(x, "+" ,y ,"=", hasil)
+
+# pembagian
+hasil = x / y
+print(x, ":" ,y ,"=", hasil)
+
+# pengurangan
+hasil = x - y
+print(x, "-" ,y ,"=", hasil)
+
+# perkalian
+hasil = x * y
+print(x, "x" ,y ,"=", hasil)
+
+# perpangkatan
+hasil = x ** y
+print(x, "^" ,y ,"=", hasil)
+
+# modulus
+hasil = x % y
+print(x, "mod" ,y ,"=", hasil)
+
+# floor division %
+hasil = x % y
+print(x, "floor" ,y ,"=", hasil)
+
+# skala prioritas (),**,*/:,+/-
+x = 3
+y = 4
+z = 5
+hasil = ( x ** y * (z + x) / y - z % x // y)
+print(hasil)
+print( x,'**', y, '*', z, '+', x, '/', y, '-', z, '%',)
