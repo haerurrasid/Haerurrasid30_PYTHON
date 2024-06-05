aaeb0bc4344bd9da1ae3eca5cbf2eab0f1ef30d2
haerurrasid <145946530+haerurrasid@users.noreply.github.com>
2024-06-05 11:11:17 +0800

    Add files via upload

---
+++ meet7&8.py
@@ -0,0 +1,16 @@
+print('berikut adalah profil beliau')
+print(30*'=')
+name = 'eddy'
+asal = 'jakarta'
+umur = 'masih muda'
+
+print(f'''profil beliau adalah\nnama = {name}\nasal = {asal}\numur = {umur}''')
+
+
+nama = input('masukan nama eddy: ')
+print(f'hallo dengan kak {nama} selamat datang')
+print(30*'=')
+
+
+    
+
