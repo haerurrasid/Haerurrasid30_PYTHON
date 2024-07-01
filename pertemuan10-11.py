0fa456a9d81dcb2ed91ac46956915a47b2eb8d08
haerurrasid <145946530+haerurrasid@users.noreply.github.com>
2024-07-02 07:37:35 +0800

    Add files via upload

---
+++ pertemuan10-11.py
@@ -0,0 +1,30 @@
+#belajar perulangan (loop)
+
+bil = 1
+print(bil)
+bil = bil + 2
+print(bil)
+bil = bil + 2
+print(bil)
+bil = bil + 2
+print(bil)
+
+#for kondisi :
+# aksi
+
+# penggunaan loop pada pemograman lain 
+# for (int i; i<=)
+
+#perulangan menggunakan list
+bil = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(bil)
+
+for i in bil :
+    print(f"nilai i sekarang -> {i}")
+    
+#perulangan mengguakan range
+bil2 = range(5)
+for i in bil :
+    print(f"nilai i sekarang -> {i}")
+    print("ahir program 2")
+
